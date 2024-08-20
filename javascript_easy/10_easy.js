afee0a2c8c9dabb4e93babc9370d94ac8c2d6fd6
DylanCollions <21oemerson@wakatipu.school.nz>
2024-08-21 11:35:35 +1200

    changed the names of flies again so users cannot cheat also added hints for the user

---
+++ javascript_easy/10_easy.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('checker').addEventListener('click', function() {
+        const correctAnswer = 'usa'; 
+        const userAnswer = document.getElementById('tbox').value.trim().toLowerCase();
+        const resultDiv = document.getElementById('result');
+
+        if (userAnswer === correctAnswer) {
+            resultDiv.textContent = 'Correct';
+            resultDiv.style.color = 'green';
+            setTimeout(function() {
+            window.location.href = '1_easy.html';
+        }, 500);
+        } else {
+            resultDiv.textContent = 'Wrong';
+            resultDiv.style.color = 'red';
+        }
+    });
+});
+
+
+function hintFunction() {
+    var x = document.getElementById("hintDIV");
+    if (x.style.display == "block") {
+      x.style.display == "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
